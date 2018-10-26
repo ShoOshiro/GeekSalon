101fb73a3cbbc6bf0416e2d18ceac98d36182ff5
大城章太 <shota@ooshiroshoutanoMacBook-Pro.local>
2018-10-26 23:31:17 +0900

    初めてのGitHub

--- Mask/ViewController.swift
+++ Mask/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var  imageView : UIImageView!
 
+    
+//    元画像の色は関係ない。
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imageView.image = UIImage(named: "image.png")?.mask(image: UIImage(named:"mask.jpg"))
     }
 
 
 }
 
+//extension UIImage {
+//    func mask(image: UIImage?) -> UIImage {
+//        if let maskRef = image?.cgImage,
+//            let ref = cgImage,
+//            let mask = CGImage(maskWidth: maskRef.width,
+//                               height: maskRef.height,
+//                               bitsPerComponent: maskRef.bitsPerComponent,
+//                               bitsPerPixel: maskRef.bitsPerPixel,
+//                               bytesPerRow: maskRef.bytesPerRow,
+//                               provider: maskRef.dataProvider!,
+//                               decode: nil,
+//                               shouldInterpolate: false),
+//            let output = ref.masking(mask) {
+//            return UIImage(cgImage: output)
+//        }
+//        return self
+//    }
+//}
+
+extension UIImage {
+    func mask(image: UIImage?) -> UIImage {
+        print(image?.cgImage)
+        if let maskRef = image?.cgImage,
+            let ref = cgImage,
+            let mask = CGImage(maskWidth: 570,
+                               height: 400,
+                               bitsPerComponent: maskRef.bitsPerComponent,
+                               bitsPerPixel: maskRef.bitsPerPixel,
+                               bytesPerRow: maskRef.bytesPerRow,
+                               provider: maskRef.dataProvider!,
+                               decode: nil,
+                               shouldInterpolate: false),
+            let output = ref.masking(mask) {
+            print(ref)
+            return UIImage(cgImage: output)
+        }
+        return self
+    }
+}
